140a50194b85b62a10eb33c16edc2c8c9dc335f4
vamshikrishna428 <vamshikrishnakr@gmail.com>
2024-01-24 16:25:13 +0530

    Hi

--- ASSIGNMENT/src/test/java/DININGHALLBOOK/SampleTest.java
+++ ASSIGNMENT/src/test/java/DININGHALLBOOK/SampleTest.java
@@ -1,4 +1,5 @@
 package DININGHALLBOOK;
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -11,54 +12,62 @@ import org.openqa.selenium.interactions.Actions;
 
 public class SampleTest {
 	public static void main(String[] args) throws InterruptedException {
-		ChromeOptions co=new ChromeOptions();
+		ChromeOptions co = new ChromeOptions();
 		co.addArguments("--disable-notifications");
-		WebDriver driver=new ChromeDriver(co);
+		WebDriver driver = new ChromeDriver(co);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://goodtaste.fleksa.de/en");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//h2[contains(text(),'MENU')]")).click();
 		WebElement dessert = driver.findElement(By.xpath("//h2[contains(text(),'Dessert')]"));
-		JavascriptExecutor j = (JavascriptExecutor)driver;
-		j.executeScript("arguments[0].scrollIntoView(true);",dessert);
-		WebElement add = driver.findElement(By.xpath("//li[@id='683211']/descendant::div[@class='add-buttoncommontemplateOnecomponents__WrapperButton-sc-uqrp0j-0 MhqSZ']/child::button"));
+		JavascriptExecutor j = (JavascriptExecutor) driver;
+		j.executeScript("arguments[0].scrollIntoView(true);", dessert);
+		WebElement add = driver.findElement(By.xpath(
+				"//li[@id='683211']/descendant::div[@class='add-buttoncommontemplateOnecomponents__WrapperButton-sc-uqrp0j-0 MhqSZ']/child::button"));
 		j.executeScript("arguments[0].click();", add);
-		WebElement dinein = driver.findElement(By.xpath("//h3[@class='text-xl font-semibold' and contains(text(),'Dine-In')]"));
+		WebElement dinein = driver
+				.findElement(By.xpath("//h3[@class='text-xl font-semibold' and contains(text(),'Dine-In')]"));
 		WebElement checkout = driver.findElement(By.xpath("//p[contains(text(),'Checkout')]"));
-		if(checkout.isDisplayed()) {
+		if (checkout.isDisplayed()) {
 			driver.findElement(By.xpath("//p[contains(text(),'Checkout')]")).click();
-		}
-		else
-		{
+		} else {
 			driver.findElement(By.xpath("//h3[@class='text-xl font-semibold' and contains(text(),'Dine-In')]")).click();
 			driver.findElement(By.xpath("//p[contains(text(),'Checkout')]")).click();
 		}
 		driver.findElement(By.xpath("//input[@class='edit-input' and @placeholder='Name']")).sendKeys("Mahesh Babu");
-		driver.findElement(By.xpath("//input[@class='edit-input' and @placeholder='Email']")).sendKeys("findmybugsqa@gmail.com");
-		Actions ac=new Actions(driver);
+		driver.findElement(By.xpath("//input[@class='edit-input' and @placeholder='Email']"))
+				.sendKeys("findmybugsqa@gmail.com");
+		Actions ac = new Actions(driver);
 
 		WebElement flag = driver.findElement(By.xpath("//div[@class='flag de']"));
 		ac.click(flag).build().perform();
 
-		WebElement code = driver.findElement(By.xpath("//ul[@class='country-list ']/child::li[@class='country']/child::span[@class='dial-code'and contains(text(),'+91')]"));
+		WebElement code = driver.findElement(By.xpath(
+				"//ul[@class='country-list ']/child::li[@class='country']/child::span[@class='dial-code'and contains(text(),'+91')]"));
 		code.click();
 		driver.findElement(By.xpath("//input[@class='form-control phone-input-style']")).sendKeys("8801356471");
 		Thread.sleep(3000);
-		WebElement edit = driver.findElement(By.xpath("//span[@class='font-semibold']/following::div[@class='edit-btn'][1]"));
+		WebElement edit = driver
+				.findElement(By.xpath("//span[@class='font-semibold']/following::div[@class='edit-btn'][1]"));
 		j.executeScript("arguments[0].click();", edit);
-		WebElement date = driver.findElement(By.xpath("//p[.='Select date']/following-sibling::div/descendant::button"));
+		WebElement date = driver
+				.findElement(By.xpath("//p[.='Select date']/following-sibling::div/descendant::button"));
 		date.click();
 		driver.findElement(By.xpath("//div[@class='relative mt-1']/child::ul/child::li[3]")).click();
-		WebElement time = driver.findElement(By.xpath("//p[.='Select time']/following-sibling::div/descendant::button"));
+		WebElement time = driver
+				.findElement(By.xpath("//p[.='Select time']/following-sibling::div/descendant::button"));
 		time.click();
 		driver.findElement(By.xpath("//div[@class='relative mt-1']/child::ul/child::li[5]")).click();
 		driver.findElement(By.xpath("//button[.='CONFIRM']")).click();
-		driver.findElement(By.xpath("//span[@class='icon-visa-credit-card px-2 border hover:border-2 hover:shadow-3xl transition-all duration-300 rounded-lg ']")).click();
-		WebElement orderpay = driver.findElement(By.xpath("//div[@class='fixed left-0 sab right-0 p-2 bg-transparent sm:relative']"));
-		j.executeScript("arguments[0].scrollIntoView(true);",orderpay);
+		driver.findElement(By.xpath(
+				"//span[@class='icon-visa-credit-card px-2 border hover:border-2 hover:shadow-3xl transition-all duration-300 rounded-lg ']"))
+				.click();
+		WebElement orderpay = driver
+				.findElement(By.xpath("//div[@class='fixed left-0 sab right-0 p-2 bg-transparent sm:relative']"));
+		j.executeScript("arguments[0].scrollIntoView(true);", orderpay);
 
+		driver.quit();
 
 	}
-
 
 }
